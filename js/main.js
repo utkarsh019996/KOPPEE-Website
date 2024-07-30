6e9701c8e7d39c4b9b0cd8b28b09da3c0637d074
Utkarsha Gouttam <152986439+utkarsh019996@users.noreply.github.com>
2024-07-30 13:16:33 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,65 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+
+   
+
+    
+})(jQuery);
+
+
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  
+  breakpoints: {
+    480: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    
+  },
+});
+swiperEl.initialize();
